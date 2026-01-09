e414bec535f3bbc0e9cf540a74c1856f278c6bfd
ije90s <idgocm@gmail.com>
2026-01-09 10:40:24 +0900

    기본 타입2

--- first.ts
+++ first.ts
@@ -70,3 +70,49 @@ let msg = "hello";
 // js 타겟은 낮은 버전으로 변환 시에는 그 버전에 맞게 js 문법으로 치환
 // 예) tsc --target es5 input.ts
 // console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+
+try {
+  // 빈 배열은 never 타입으로 인식하기 때문에, 타입 명시
+  const array = [];
+  //array[0];
+} catch (error) {
+  error;
+}
+
+// non-null assertion : ! > 되도록이면 안쓰는 게 나음
+// 두번째 방법으로 값이 있는지 검증해서 쓰는 게 낫다
+const head = document.querySelector("#head")!;
+console.log(head);
+
+const head2 = document.querySelector("#head");
+if (head2) {
+  // 오타 판별
+  head2.inneRHTML = "hello";
+  console.log(head);
+}
+
+// 원시 타입은 되도록이면 소문자 명시
+const a1: string = "hello";
+const b1: String = "hell"; // new String();
+
+function c(a1: string, b2: string) {}
+c(a1, b1);
+
+// 템플릿 리터럴 타입 > 자동 완성 추천
+type World = "world" | "hell";
+const a2: World = "world";
+
+// 템플릿 리터럴 타입 없이 한다면, 모든 문자열 가능
+type Greeting = `hello ${string}`;
+const c2: Greeting = "hello";
+
+let arr4: string[] = [];
+let arr5: Array<string> = [];
+function rest(a: string, ...args: string[]) {
+  console.log(a, args);
+}
+rest("1", "2", "3");
+
+const tuple: [string, number] = ["1", 1];
+tuple[2] = "hello"; // 불가능
+tuple.push("hello"); // 가능
