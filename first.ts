c5d75457c6821de52ee3cad91952526ee975a35b
ije90s <idgocm@gmail.com>
2026-03-06 17:29:27 +0900

    제너릭

--- first.ts
+++ first.ts
@@ -420,3 +420,21 @@ class Y extends X {
     return true;
   }
 }
+
+function abc(a: number, b?: number, c: number?) {}
+abc(1)
+abc(1, 2)
+abc(1, 2, 3)
+
+let obj: { a: string, b?: string }  = { a: 'hello', b: 'world' }
+obj = { a: 'hello' };
+
+
+// 제너릭
+// 제한: extends
+function add<T extends number, K extends string>(x: T, y: K): T { return x + y }
+add<number>(1, 2);
+add(1, 2);
+add<string>('1', '2');
+add('1', '2');
+add(1, '2');
